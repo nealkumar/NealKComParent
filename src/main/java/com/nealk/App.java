75d9ddaa88263bb59362bc5bc3c49106fd7f77ef
Neal Kumar <nkumar20@jcu.edu>
2020-02-14 13:10:45 -0500

    initial commit

---
+++ src/main/java/com/nealk/App.java
@@ -0,0 +1,13 @@
+package com.nealk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nealk/AppTest.java
@@ -0,0 +1,20 @@
+package com.nealk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
